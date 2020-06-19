c4141c82e85c2d5a8499fec4cc9b3d2fda17f2dd
lbaires <francisco.baires@claro.com.sv>
2020-06-19 10:50:31 -0600

    Mobile Apps upload

---
+++ mobileapp/driver/Online/Source/Source/config.js
@@ -0,0 +1,6 @@
+var secret_key = "My_key";
+var map_api_key="AIzaSyBoL26hVGudbJBDMo5yT3PIc-A93EX67NE";
+var server_domain = 'http://tchwr.in/NewCallmycab';
+//var server_domain = 'http://192.168.1.31/shajeer/cmc_website/Source';
+//var server_domain = 'http://192.168.138.31/lidiya/callmycab';
+var wordpress = false;
